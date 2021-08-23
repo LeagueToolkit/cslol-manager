0aa2c984b4dbba40197708d7494103aa97d77669
moonshadow565 <moonshadow565@hotmail.com>
2021-08-23 17:15:21 +0200

    fix asm build script

--- lcs-patcher/asm/build.sh
+++ lcs-patcher/asm/build.sh
@@ -3,9 +3,9 @@ nasm -f bin -Ox -o file_hook.bin file_hook.asm
 nasm -f bin -Ox -o up_hook.bin up_hook.asm
 nasm -f bin -Ox -o macos_hook.bin macos_hook.asm
 
-xxd -i -C -c 8 file_hook.bin
-xxd -i -C -c 8 up_hook.bin
-xxd -i -C -c 8 macos_hook.bin
+xxd -i -c 8 file_hook.bin
+xxd -i -c 8 up_hook.bin
+xxd -i -c 8 macos_hook.bin
 
 rm file_hook.bin
 rm up_hook.bin
