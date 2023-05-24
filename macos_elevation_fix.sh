74407ed994a4ba3a33791a062bcf3931dfd9dae2
kernel-dev <twitchisadeck@gmail.com>
2023-05-24 15:24:35 +0200

    fix: self-close AppleScript window

--- macos_elevation_fix.sh
+++ macos_elevation_fix.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 osascript <<EOD
-    do shell script "sudo $SCRIPT_DIR/cslol-manager" \
+    do shell script "sudo $SCRIPT_DIR/cslol-manager >/dev/null 2>&1 &" \
     with prompt "CSLoL-Manager needs admin privileges to restart" \
     with administrator privileges \
     with altering line endings
